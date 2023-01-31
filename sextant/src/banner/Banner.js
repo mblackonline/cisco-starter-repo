c4eb238d659291a986741d95317eed966c0d5dc2
Matt Black <mblackonline.git@gmail.com>
2023-01-31 10:18:01 -0500

    created a banner and added containers for data points (task 2)

---
+++ sextant/src/banner/Banner.js
@@ -0,0 +1,11 @@
+import "./banner-styling.css";
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <h2>Sextant Dashboard</h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ sextant/src/exhibit/Exhibit.js
@@ -0,0 +1,35 @@
+import "./Exhibit.css";
+
+const Exhibit = () => {
+    return (
+        <div>
+            <div className="exhibit">
+                <h4>Data List1</h4>
+                <ul>
+                <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+            <div className="exhibit">
+                <h4>Data List2</h4>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+            <div className="exhibit">
+                <h4>Data List3</h4>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Exhibit;
+
