03169d39bc2b40e57761afd5b259c521d8651c54
Matt Black <mblackonline.git@gmail.com>
2023-01-31 15:52:11 -0500

    updated exhibit layout

--- sextant/src/exhibit/Exhibit.js
+++ sextant/src/exhibit/Exhibit.js
@@ -6,25 +6,19 @@ const Exhibit = () => {
             <div className="exhibit">
                 <h4>Data List1</h4>
                 <ul>
-                <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
+                    <li>Item 1</li>
                 </ul>
             </div>
             <div className="exhibit">
                 <h4>Data List2</h4>
                 <ul>
                     <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
                 </ul>
             </div>
             <div className="exhibit">
                 <h4>Data List3</h4>
                 <ul>
                     <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
                 </ul>
             </div>
         </div>
