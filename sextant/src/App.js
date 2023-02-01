0435d571ce5b8ae941452b3911b6c2e498853e70
Matt Black <mblackonline.git@gmail.com>
2023-02-01 16:19:02 -0500

    updated the App.js, Banner.js, and Exhibit.js files using the model answers as guides. Also reworked the CSS files.

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,15 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
-import Banner from "./banner/Banner";
-import Exhibit from "./exhibit/Exhibit";
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
-
-function App() {
-  return (
-      <div className="App">
-          <Banner />
-          <Exhibit />
-      </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Banner bannerText="Sextant Dashboard" />
+                <Exhibit name="ipv4"></Exhibit>
+                <Exhibit name="ipv6"></Exhibit>
+                <Exhibit name="Packet Latency"></Exhibit>
+            </div>
+        );
+    }
 }
 
 export default App;
